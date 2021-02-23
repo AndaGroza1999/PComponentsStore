feb0f1dbcc6232a45258c706fadfb57c3e7aa23c
Anda <andagroza1999@gmail.com>
2021-02-23 17:50:15 +0200

    + product model

--- src/main/java/com/PComponents/model/Product.java
+++ src/main/java/com/PComponents/model/Product.java
@@ -1,4 +1,37 @@
 package com.PComponents.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PRODUCT_ID")
+    private Long idProduct;
+
+    @Column(name = "PRODUCT_NAME")
+    private String productName;
+
+    @Column(name = "PRODUCT_PRICE")
+    private Double productPrice;
+
+    //daca e in stock sau nu
+    @Column(name = "IS_IN_STOCK")
+    private boolean isInStock;
+
+    @Column(name = "PRODUCT_INFORMATION")
+    private String productInformation;
+
 }
